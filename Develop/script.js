2f365b28375aaa284095e32a982ad39a1971663b
Karl Brewster <mark.moehring@outlook.com>
2022-03-31 13:18:40 -0500

    add first steps for criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
+var criteria = function() {
+  alert("First, let's determine the criteria for your new password.");
+  prompt("How many characters long should your password be?  Please enter a number between from 8-128.");
 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +18,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", criteria);
