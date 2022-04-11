e0ee70958a4b138161ecea07819a69640cb061d3
Karl Brewster <mark.moehring@outlook.com>
2022-04-10 23:16:35 -0500

    Good updates in logic and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,79 @@
 // Assignment code here
 
-
-var production = function generatePassword() {
-  var inputLengthEl = window.prompt("Ok, first, let's determine what criteria your password will need to meet. \nEnter your desired password length of characters from 8-128.");
-
-  if (inputLengthEl) {
+function generatePassword() {
+  
+    var criteriaConfirm = window.confirm("Ok, first, let's determine what criteria your password will need to meet.");
     
-    console.log(inputLengthEl);
+    //debugger
+    // if (!criteriaConfirm) {
+    //   exit();
+    // } else {
     
+    // Beginning of length prompt  
+    function inputPrompt() {
+        let inputLength = window.prompt("Enter your desired number of characters from 8-128.");
+      
+          if (inputLength) {
+            console.log(inputLength);
+                        
+            function inputParse() {
+              var length = parseInt(inputLength);
+        
+              console.log(length);
+              if (length > 7 && length < 129) {
+                console.log("Good number")
+
+              } else {
+                console.log("Ooh, bad pick.")
+                function uhuh() {
+                  window.alert("You must enter a number from 8-128.");
+                }
+                uhuh();
+                inputPrompt();
+              }
+            }
+            inputParse();
+          } else {
+            window.alert("You must enter a number from 8-128.");
+            inputPrompt();
+            }
+      }
+    //End of length prompt
+      inputPrompt();
+
+    //}     
+
+  
+    //Beginning of character type prompts
+    function inputChar() {
+      window.alert("Next, please select from the following prompts the kinds of characters you would like to include.\nSelect either 'OK' to include the character, or 'Cancel' to skip to the next selection.");
+      window.confirm("Include uppercase letters? (e.g. N,E,B,A,K,J)");
+      window.confirm("Include lowercase letters? (e.g. f,o,a,i,u,p)");
+      window.confirm("Include numbers? (e.g. 0,5,9,3,8,7)");
+      window.confirm("And lastly, include special characters?\n(e.g. #,$,%,&,@,^)");
+    
+    }
+    inputChar();
+
+    //End of character prompts
+    var criteriaResult = "oaLKJ)&/97LYD*&"
+    return criteriaResult;
+};    
+function exit() {
+  confirm("Are you sure you'd like to exit out?")
+
+  if (!confirm) {
+    generatePassword();
+
   } else {
-    window.alert("uh-uh")
+    alert("Ok, thanks for stopping by!");
   }
+}
 
-  var inputChar = window.prompt("What kind of characters would you like to include?")
-  
-    
-    
-  //   var lengthConfirm = (8 <= inputLength <= 128);
-  //   console.log(inputLength);
-  //   console.log("Good number");
-  // } else {
-  //     console.log("Nope!  Try again.");
-  //     alert("You must enter a number from 8-128.");
-  //     generatePassword();
-  // }
-  
-  
+
   
 
   
-   
-};
-  
-//generatePassword()  
 //var specchar = !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 
 
@@ -49,4 +90,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", production);
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Password Generator Starter Code
+# Password Generator 
+## Description 
+Looking to shore up your security?
+**Of course you are!**
+Have no fear, with this state-of-the-art password generator, you'll be able to customize your own virtually unsinkable password up to 128 characters long!
+
+Live URL: [Password Generator]()
+GitHub repo: [password-generator](https://github.com/MarkMoehring/password-generator)
